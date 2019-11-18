da6ff5227964c5eaf4f60ab5985408e3039a699d
Vu Luu <vu.luu@messagemedia.com>
2019-11-18 13:53:44 +0700

    support antd themes

--- src/App.js
+++ src/App.js
@@ -7,59 +7,102 @@ import {
   DatePicker,
   Switch,
   Slider,
-  Button
+  Button,
+  message
 } from "antd"
 import * as styles from "./App.less"
 
 const { Option } = Select
 
-const App = () => (
-  <div className={styles.form}>
-    <Form>
-      <Form.Item
-        label="数字输入框"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-      >
-        <InputNumber min={1} max={10} defaultValue={3} />
-        <span className="ant-form-text"> 台机器</span>
-        <a href="https://ant.design">链接文字</a>
-      </Form.Item>
-      <Form.Item label="开关" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-        <Switch defaultChecked />
-      </Form.Item>
-      <Form.Item
-        label="滑动输入条"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-      >
-        <Slider defaultValue={70} />
-      </Form.Item>
-      <Form.Item label="选择器" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-        <Select defaultValue="lucy" style={{ width: 192 }}>
-          <Option value="jack">jack</Option>
-          <Option value="lucy">lucy</Option>
-          <Option value="disabled" disabled>
-            disabled
-          </Option>
-          <Option value="yiminghe">yiminghe</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="日期选择框"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-      >
-        <DatePicker />
-      </Form.Item>
-      <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          确定
-        </Button>
-        <Button style={{ marginLeft: 8 }}>取消</Button>
-      </Form.Item>
-    </Form>
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    const initialValue = {
+      "@primary-color": "#1987a7",
+      "@secondary-color": "#0000ff",
+      "@text-color": "#000000",
+      "@text-color-secondary": "#eb2f96",
+      "@heading-color": "#fa8c16",
+      "@layout-header-background": "#b36e94",
+      "@btn-primary-bg": "#397dcc"
+    }
+    let vars = {}
+
+    try {
+      vars = {
+        ...initialValue,
+        ...JSON.parse(localStorage.getItem("app-theme"))
+      }
+    } finally {
+      // eslint-disable-next-line react/no-unused-state
+      this.state = { vars, initialValue }
+      window.less
+        .modifyVars(vars)
+        .then(() => {})
+        .catch(error => {
+          message.error(`Failed to update theme`, error)
+        })
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.form}>
+        <Form>
+          <Form.Item
+            label="数字输入框"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+          >
+            <InputNumber min={1} max={10} defaultValue={3} />
+            <span className="ant-form-text"> 台机器</span>
+            <a href="https://ant.design">链接文字</a>
+          </Form.Item>
+          <Form.Item
+            label="开关"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+          >
+            <Switch defaultChecked />
+          </Form.Item>
+          <Form.Item
+            label="滑动输入条"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+          >
+            <Slider defaultValue={70} />
+          </Form.Item>
+          <Form.Item
+            label="选择器"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+          >
+            <Select defaultValue="lucy" style={{ width: 192 }}>
+              <Option value="jack">jack</Option>
+              <Option value="lucy">lucy</Option>
+              <Option value="disabled" disabled>
+                disabled
+              </Option>
+              <Option value="yiminghe">yiminghe</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="日期选择框"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+          >
+            <DatePicker />
+          </Form.Item>
+          <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              确定
+            </Button>
+            <Button style={{ marginLeft: 8 }}>取消</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
+}
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,38 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const TerserJSPlugin = require("terser-webpack-plugin")
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const AntDesignThemePlugin = require("antd-theme-webpack-plugin")
+
+const options = {
+  antDir: path.join(__dirname, "./node_modules/antd"),
+  stylesDir: path.join(__dirname, "./src/styles"),
+  varFile: path.join(__dirname, "./src/styles/variables.less"),
+  mainLessFile: path.join(__dirname, "./src/styles/theme.less"),
+  themeVariables: [
+    "@primary-color",
+    "@secondary-color",
+    "@text-color",
+    "@text-color-secondary",
+    "@heading-color",
+    "@layout-body-background",
+    "@btn-primary-bg",
+    "@layout-header-background"
+  ],
+  indexFileName: "index.html",
+  generateOnce: false,
+  lessUrl: "https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js",
+  publicPath: ""
+}
 
 module.exports = {
   output: {
     path: path.join(__dirname, "./build"),
     publicPath: "/"
   },
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM"
+  // },
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
     splitChunks: {
@@ -106,6 +132,7 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
       ignoreOrder: false // Enable to remove warnings about conflicting order
-    })
+    }),
+    new AntDesignThemePlugin(options)
   ]
 }
